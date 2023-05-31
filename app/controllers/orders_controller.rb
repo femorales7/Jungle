4fa2de558914c93cc87abda3144a3c910c45d293
Your Name <your@email.com>
2023-05-31 16:55:40 -0600

    improve the performance after placed order with summary of order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @order_total = @order.line_items.sum { |line_item| line_item.product.price * line_item.quantity }
   end
 
   def create
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,10 @@ class LineItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
+  def total_price
+    product.price * quantity
+  end
+
   monetize :item_price_cents, numericality: true
   monetize :total_price_cents, numericality: true
 
